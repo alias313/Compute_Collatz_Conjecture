405846f377bf3ffa6855190ec55c4634a4a87300
alias313 <eliesgarciaalvira@gmail.com>
2023-10-01 02:31:27 +0200

    Added cli support with error handling

--- Collatz_Compute.py
+++ Collatz_Compute.py
@@ -2,6 +2,7 @@
 This algorithm takes a number n. If n is even, n is multiplied by 0.5. If n is odd, it's multiplied by 3 and it adds one 
 to the result.
 """
+import sys
 
 def show_collatz(n):
     """This functions shows the steps n takes to get to 1 and what the steps are"""
@@ -24,7 +25,7 @@ def show_collatz(n):
         {"-"*(30)}
         """)
 
-def collatz_range_n(n):
+def collatz_range(n):
     """This function takes in a number and shows all the numbers from 1 to n 
     and shows how many steps each number takes to get to 1"""
     for i in range(1, n+1):
@@ -39,7 +40,7 @@ def collatz_range_n(n):
         print(f"{i}: {j} steps")
 
 
-def collatz_range(n, a):
+def collatz_specific_range(n, a):
     """This function does the same as collatz_range but this time from n to n+a"""
     for i in range(n, n+a):
         j = 0
@@ -51,3 +52,42 @@ def collatz_range(n, a):
                 1: int(3*c + 1)
             }.get(int(c%2))
         print(f"{i}: {j} steps")
+
+if __name__ == "__main__":
+    try:
+        arg1 = sys.argv[1:]
+        arg2 = sys.argv[2]
+    except IndexError:
+        raise SystemExit(f"Usage: {sys.argv[0]} <function name> <argument/s>")
+    
+    try:
+        arg2 = int(arg2)
+        if len(sys.argv) == 4:
+            arg3 = int(sys.argv[3])
+            if arg3 < 0:
+                raise SystemExit("Please enter a positive integer as an argument")
+    except ValueError:
+        raise SystemExit("Please enter an integer as the second and/or third command-line argument")
+    
+    if arg2 < 0:
+        raise SystemExit("Please enter a positive integer as an argument")
+
+    if arg1 == "show_collatz":
+        if len(sys.argv) == 3:
+            show_collatz(arg2)
+        else:
+            raise SystemExit("show_collatz accepts one integer argument")
+    elif arg1 == "collatz_range":
+        if len(sys.argv) == 3:
+            collatz_range(arg2)
+        else:
+            raise SystemExit("collatz_range accepts one integer argument")
+    elif arg1 == "collatz_specific_range":
+        if len(sys.argv) == 4 and arg2 < arg3:
+            collatz_specific_range(arg2, arg3)
+        else:
+            raise SystemExit("collatz_specific_range accepts two integer arguments, the first smaller than the second")
+    else:
+        raise SystemExit("Please enter a valid function name")
+
+
